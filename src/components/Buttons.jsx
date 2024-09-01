81c0013adce9fa766ff0c51d6d4dc9aec4d14be5
Halid Besic <halidstl@gmail.com>
2024-09-01 15:46:04 -0500

    completed assigment

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,19 @@
-import { satData } from "./components/satData";
-import "styling.css";
+import satData from "./satData";
+import './styling.css';
 
 const Buttons = ({ filterByType, setSat, orbitTypes }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className = "flex-container">
+      {orbitTypes.map((type, index) => {
+  return (
+    <button onClick={() => filterByType(type)} key = {index}>
+    {type} Orbit
+    </button>
+  )
+})}
+      <button onClick={() => setSat(satData)}>
+        All Orbits
+        </button>
     </div>
   );
 };
